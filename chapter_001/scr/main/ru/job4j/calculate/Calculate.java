c9bc563d161a896d5beca0dfd0de031cc0221d13
Smerigl <Failong@mail.ru>
2018-08-06 15:43:52 +0300

    #4087 1.3 JavaDoc. Code Convention

--- chapter_001/scr/main/ru/job4j/calculate/Calculate.java
+++ chapter_001/scr/main/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
+/**
+Package for calculate task
+@autor Oleg Karpaev  (Failong@mail.ru)
+@version 1
+@since 02. 08.18
+*/
 package ru.job4j.calculate;
-
 public class Calculate{
+/**
+*@param args - args
+*@return Hello World
+*/
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
